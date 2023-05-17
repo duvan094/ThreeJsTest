9332db842a81de6e6499e5574110e931b3355ee1
Jacob Duvander <jacobduvander@gmail.com>
2023-05-17 20:01:44 +0200

    small typo

--- src/components/DemoInfo.vue
+++ src/components/DemoInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <p>Playing around with <a href="https://threejs.org/" target="_blank" rel="noopener noreferrer">three.js</a>. The discount "Shrek" model was made by <a href="https://jacobduvander.se/about" target="_blank">me</a> when trying out <a href="https://www.blender.org/" target="_blank" rel="noopener noreferrer">blender</a> 🧅</p>
+        <p>Playing around with <a href="https://threejs.org/" target="_blank" rel="noopener noreferrer">three.js</a>. The discount "Shrek" model was made by <a href="https://jacobduvander.se/about" target="_blank">me</a> when trying out <a href="https://www.blender.org/" target="_blank" rel="noopener noreferrer">Blender</a> 🧅</p>
     </div>
 </template>
   
